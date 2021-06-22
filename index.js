375ad27fc26daae2c59a6fcee3a1ade821bb12d3
cervantes2907 <uvaldocervantes57>
2021-06-22 00:24:39 -0500

    creando archivo presentacion

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+document.getElementById("s1").innerHTML = `
+<h1>Hola!</h1>
+<div>
+  Me llamo ubaldo tengo 29 años  soy de colombia  y vivo en 
+  la ciudad de medellin con mi familia.
+</div>
+`;
+
+document.getElementById("s2").innerHTML = `
+<h1>intereses!</h1>
+<div>
+ tecnologias nuevas, video juegos, viajes,
+ empleos en grandes empresas etc
+</div>
+`;
+
+document.getElementById("s3").innerHTML = `
+<h1>hobbies!</h1>
+<div>
+  Me gusta ver animes, video juegos, escuchar musica, bailar,
+  leer, pasar tiempo con mi familia etc... 
+</div>
+`;
+
+document.getElementById("s4").innerHTML = `
+<h1>Que me gustaria aprender!</h1>
+<div>
+Me gustaria aprender ingles y aleman tambien me 
+encantaria aprender muchas tecnologias para programar
+</div>
+`;
+
+document.getElementById("s5").innerHTML = `
+<h1>espectativas!</h1>
+<div>
+mis espectativas actualmente es terminar este curso con academlo 
+para asi poder conseguir un buen empleo y luego disfrutar todo 
+lo que pueda con mi familia
+</div>
+`;
